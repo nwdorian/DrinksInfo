35ad119f2b99adfafbacf5f17ae6810d0a866b19
nwdorian <neuwirthdorian@gmail.com>
2024-05-21 17:23:34 +0200

    feat: add drink categories and drink details functionality

--- DrinksInfo/Program.cs
+++ DrinksInfo/Program.cs
@@ -1,2 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using DrinksInfo;
+
+UserInput userInput = new();
+await userInput.GetCategoriesInput();
---
+++ DrinksInfo/UserInput.cs
@@ -0,0 +1,52 @@
+﻿using Spectre.Console;
+
+namespace DrinksInfo;
+internal class UserInput
+{
+    DrinksService drinksService = new();
+
+    internal async Task GetCategoriesInput()
+    {
+        var categories = await drinksService.GetCategories();
+
+        string category = AnsiConsole.Prompt(
+            new TextPrompt<string>("Choose category: ")
+            .ValidationErrorMessage("[red]Invalid category[/]")
+            .Validate(Validation.IsStringValid));
+
+        if (!categories.Any(x => x.strCategory.ToLower() == category.ToLower()))
+        {
+            AnsiConsole.MarkupLine("[red]Category doesn't exist![/]");
+            await GetCategoriesInput();
+        }
+
+        await GetDrinksInput(category);
+    }
+
+    internal async Task GetDrinksInput(string category)
+    {
+        var drinks = await drinksService.GetDrinksByCategory(category);
+
+        string drink = AnsiConsole.Prompt(
+            new TextPrompt<string>("Choose a drink (press 0 to go back to categories): ")
+            .ValidationErrorMessage("[red]Invalid Id[/]")
+            .Validate(Validation.IsIdValid));
+
+        if (drink == "0")
+        {
+            await GetCategoriesInput();
+        }
+
+        if (!drinks.Any(x => x.idDrink == drink))
+        {
+            AnsiConsole.MarkupLine("[red]Drink doesn't exist![/]");
+            await GetDrinksInput(category);
+        }
+
+        await drinksService.GetDrink(drink);
+
+        AnsiConsole.Write("Press any key to go back to categories menu... \n");
+        Console.ReadKey();
+        if (!Console.KeyAvailable) await GetCategoriesInput();
+    }
+}
